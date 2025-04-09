4224f0bd64619776cf573c229fb7f54af8c94599
Michael Pacheco <mdpgd95@gmail.com>
2025-04-08 23:38:58 -0300

    chore: setup prettier

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -91,7 +91,7 @@ const config: Config = {
           position: "right",
         },
         {
-          type: 'localeDropdown',
+          type: "localeDropdown",
         },
       ],
     },
@@ -148,11 +148,11 @@ const config: Config = {
 
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
 };
